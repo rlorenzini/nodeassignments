dd842b10d123b3e14a4b34c3ea74cdcade410349
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-04-30 10:19:27 -0500

    react-redux counter

---
+++ reactlearning/react-redux-hello/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter.js';
+
+function App() {
+  return (
+    <div>
+    <h1>Hello</h1>
+    <Counter/>
+    </div>
+  );
+}
+
+export default App;
